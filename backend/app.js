c6d49c17b8738bc72275caf33b09f6d2f06d79f1
vicente olivares <vicenteoli93@gmail.com>
2019-01-29 07:56:10 -0600

    update

--- backend/app.js
+++ backend/app.js
@@ -4,4 +4,13 @@ let express = require('express');
 
 let app = express();
 
+//LOAD ROUTES
+let UserRoutes = require('./routes/user');
+
+
+//HEADERS CONFIGURATIONS (CORS)
+
+
+app.use('/system', UserRoutes);
+
 module.exports = app;
