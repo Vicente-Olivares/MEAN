9ea7fce53cd3586b309da2661af6de2d5fbe8106
vicente olivares <vicenteoli93@gmail.com>
2019-01-30 23:05:53 -0600

    update

--- backend/app.js
+++ backend/app.js
@@ -2,19 +2,10 @@
 
 let express = require('express');
 let bodyParser = require('body-parser');
-<<<<<<< HEAD
 let app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-
-=======
-
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
-let app = express();
->>>>>>> d09e9c5df3f5a6a14f16bac319991124cc857c4d
 
 //LOAD ROUTES
 let UserRoutes = require('./routes/user');
